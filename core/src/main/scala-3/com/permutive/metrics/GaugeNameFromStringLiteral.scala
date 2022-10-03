231107dbe8798e242d797d7f1fe3fa2779c8f7de
Chris Jansen <janstenpickle@users.noreply.github.com>
2022-10-03 11:45:31 +0100

    scalafmt

--- core/src/main/scala-3/com/permutive/metrics/GaugeNameFromStringLiteral.scala
+++ core/src/main/scala-3/com/permutive/metrics/GaugeNameFromStringLiteral.scala
@@ -4,7 +4,9 @@ import scala.quoted.*
 
 trait GaugeNameFromStringLiteral {
 
-  implicit inline def fromStringLiteral(inline t: String): Gauge.Name = ${GaugeNameFromStringLiteral.nameLiteral('t)}
+  implicit inline def fromStringLiteral(inline t: String): Gauge.Name = ${
+    GaugeNameFromStringLiteral.nameLiteral('t)
+  }
 
 }
 
@@ -12,9 +14,21 @@ object GaugeNameFromStringLiteral {
   def nameLiteral(s: Expr[String])(using q: Quotes): Expr[Gauge.Name] =
     s.value match {
       case Some(string) =>
-        Gauge.Name.from(string).fold(e => throw new RuntimeException(e), _ => '{Gauge.Name.from( ${Expr(string)}).fold(e => throw new RuntimeException(e), identity)})
+        Gauge.Name
+          .from(string)
+          .fold(
+            e => throw new RuntimeException(e),
+            _ =>
+              '{
+                Gauge.Name
+                  .from(${ Expr(string) })
+                  .fold(e => throw new RuntimeException(e), identity)
+              }
+          )
       case None =>
-        q.reflect.report.error("This method uses a macro to verify that a Name literal is valid. Use Gauge.Name.from if you have a dynamic value you want to parse as a name.")
-        '{???}
+        q.reflect.report.error(
+          "This method uses a macro to verify that a Name literal is valid. Use Gauge.Name.from if you have a dynamic value you want to parse as a name."
+        )
+        '{ ??? }
     }
 }
--- core/src/main/scala-3/com/permutive/metrics/MetricHelpFromStringLiteral.scala
+++ core/src/main/scala-3/com/permutive/metrics/MetricHelpFromStringLiteral.scala
@@ -4,7 +4,9 @@ import scala.quoted.*
 
 trait MetricHelpFromStringLiteral {
 
-  implicit inline def fromStringLiteral(inline t: String): Metric.Help = ${MetricHelpFromStringLiteral.nameLiteral('t)}
+  implicit inline def fromStringLiteral(inline t: String): Metric.Help = ${
+    MetricHelpFromStringLiteral.nameLiteral('t)
+  }
 
 }
 
@@ -12,9 +14,21 @@ object MetricHelpFromStringLiteral {
   def nameLiteral(s: Expr[String])(using q: Quotes): Expr[Metric.Help] =
     s.value match {
       case Some(string) =>
-        Metric.Help.from(string).fold(e => throw new RuntimeException(e), _ => '{Metric.Help.from( ${Expr(string)}).fold(e => throw new RuntimeException(e), identity)})
+        Metric.Help
+          .from(string)
+          .fold(
+            e => throw new RuntimeException(e),
+            _ =>
+              '{
+                Metric.Help
+                  .from(${ Expr(string) })
+                  .fold(e => throw new RuntimeException(e), identity)
+              }
+          )
       case None =>
-        q.reflect.report.error("This method uses a macro to verify that a Name literal is valid. Use Gauge.Name.from if you have a dynamic value you want to parse as a name.")
-        '{???}
+        q.reflect.report.error(
+          "This method uses a macro to verify that a Name literal is valid. Use Gauge.Name.from if you have a dynamic value you want to parse as a name."
+        )
+        '{ ??? }
     }
 }
