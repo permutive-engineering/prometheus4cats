7f9d319a825da126ec743fd4cd7d8bd3fbd81a13
Tim Spence <timothywspence@gmail.com>
2022-12-06 12:52:58 +0000

    Implement createAndRegisterDoubleCounter

---
+++ testing/src/main/scala/prometheus4cats/testing/TestingMetricRegistry.scala
@@ -0,0 +1,108 @@
+package prometheus4cats
+package testing
+
+import cats.syntax.all._
+import prometheus4cats.util.DoubleMetricRegistry
+import cats.effect.kernel._
+import cats.effect.std.MapRef
+import cats.data.NonEmptySeq
+import scala.concurrent.duration.FiniteDuration
+
+class TestingMetricRegistry[F[_]](
+    private val counters: MapRef[F, (Counter.Name, Metric.CommonLabels), Option[(Int, Counter[F, Double])]]
+)(implicit F: Concurrent[F])
+    extends DoubleMetricRegistry[F] {
+
+  override protected[prometheus4cats] def createAndRegisterDoubleCounter(
+      prefix: Option[Metric.Prefix],
+      name: Counter.Name,
+      help: Metric.Help,
+      commonLabels: Metric.CommonLabels
+  ): Resource[F, Counter[F, Double]] =
+    Resource
+      .eval(F.ref(0.0).flatMap { ref =>
+        val release =
+          counters(name -> commonLabels).update {
+            case None => throw new RuntimeException("This should be unreachable - our reference counting has a bug")
+            case Some((n, c)) => if (n == 1) None else Some(n - 1 -> c)
+          }
+
+        counters(name -> commonLabels).modify {
+          case None =>
+            val counter = Counter.make[F, Double](d => ref.set(d))
+            Some(1 -> counter) -> F.pure(
+              Resource.make(F.pure(counter))(_ => release)
+            )
+          case Some((n, c)) => Some(n + 1 -> c) -> F.pure(Resource.make(F.pure(c))(_ => release))
+        }.flatten
+      })
+      .flatten
+
+  override protected[prometheus4cats] def createAndRegisterLabelledDoubleCounter[A](
+      prefix: Option[Metric.Prefix],
+      name: Counter.Name,
+      help: Metric.Help,
+      commonLabels: Metric.CommonLabels,
+      labelNames: IndexedSeq[Label.Name]
+  )(f: A => IndexedSeq[String]): Resource[F, Counter.Labelled[F, Double, A]] = ???
+
+  override protected[prometheus4cats] def createAndRegisterDoubleGauge(
+      prefix: Option[Metric.Prefix],
+      name: Gauge.Name,
+      help: Metric.Help,
+      commonLabels: Metric.CommonLabels
+  ): Resource[F, Gauge[F, Double]] = ???
+
+  override protected[prometheus4cats] def createAndRegisterLabelledDoubleGauge[A](
+      prefix: Option[Metric.Prefix],
+      name: Gauge.Name,
+      help: Metric.Help,
+      commonLabels: Metric.CommonLabels,
+      labelNames: IndexedSeq[Label.Name]
+  )(f: A => IndexedSeq[String]): Resource[F, Gauge.Labelled[F, Double, A]] = ???
+
+  override protected[prometheus4cats] def createAndRegisterDoubleHistogram(
+      prefix: Option[Metric.Prefix],
+      name: Histogram.Name,
+      help: Metric.Help,
+      commonLabels: Metric.CommonLabels,
+      buckets: NonEmptySeq[Double]
+  ): Resource[F, Histogram[F, Double]] = ???
+
+  override protected[prometheus4cats] def createAndRegisterLabelledDoubleHistogram[A](
+      prefix: Option[Metric.Prefix],
+      name: Histogram.Name,
+      help: Metric.Help,
+      commonLabels: Metric.CommonLabels,
+      labelNames: IndexedSeq[Label.Name],
+      buckets: NonEmptySeq[Double]
+  )(f: A => IndexedSeq[String]): Resource[F, Histogram.Labelled[F, Double, A]] = ???
+
+  override protected[prometheus4cats] def createAndRegisterDoubleSummary(
+      prefix: Option[Metric.Prefix],
+      name: Summary.Name,
+      help: Metric.Help,
+      commonLabels: Metric.CommonLabels,
+      quantiles: Seq[Summary.QuantileDefinition],
+      maxAge: FiniteDuration,
+      ageBuckets: Summary.AgeBuckets
+  ): Resource[F, Summary[F, Double]] = ???
+
+  override protected[prometheus4cats] def createAndRegisterLabelledDoubleSummary[A](
+      prefix: Option[Metric.Prefix],
+      name: Summary.Name,
+      help: Metric.Help,
+      commonLabels: Metric.CommonLabels,
+      labelNames: IndexedSeq[Label.Name],
+      quantiles: Seq[Summary.QuantileDefinition],
+      maxAge: FiniteDuration,
+      ageBuckets: Summary.AgeBuckets
+  )(f: A => IndexedSeq[String]): Resource[F, Summary.Labelled[F, Double, A]] = ???
+
+  override protected[prometheus4cats] def createAndRegisterInfo(
+      prefix: Option[Metric.Prefix],
+      name: Info.Name,
+      help: Metric.Help
+  ): Resource[F, Info[F, Map[Label.Name, String]]] = ???
+
+}
