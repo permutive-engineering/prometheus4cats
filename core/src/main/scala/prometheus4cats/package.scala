818ab1e6bd1e801807526a02e657463f6cdd23bf
Alejandro Hernández <info@alejandrohdezma.com>
2023-08-14 16:49:03 +0200

    Fix compilation errors on Scala 2.12

--- core/src/main/scala/prometheus4cats/package.scala
+++ core/src/main/scala/prometheus4cats/package.scala
@@ -14,12 +14,14 @@
  * limitations under the License.
  */
 
+import scala.annotation.nowarn
+
 import prometheus4cats.internal.ShapelessPolyfill
 
 package object prometheus4cats extends ShapelessPolyfill {
   def unexpected: Nothing = sys.error("Unexpected invocation")
 
-  trait =:!=[A, B] extends Serializable
+  @nowarn trait =:!=[A, B] extends Serializable
 
   implicit def neq[A, B]: A =:!= B = new =:!=[A, B] {}
 
